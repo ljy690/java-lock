ab1c4ba7ab6b2aea668e85c2ecf673b6f04bcc2e
liaojy <657513865@qq.com>
2019-02-18 11:18:23 +0800

    add classes optimisticLock and pressmicLock

--- src/main/java/cn/com/concept/OptiLock.java
+++ src/main/java/cn/com/concept/OptiLock.java
@@ -1,4 +1,20 @@
 package main.java.cn.com.concept;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 乐观锁：线程不锁住同步资源。
+ * 乐观锁认为自己在使用数据时不会有别的线程修改数据，所以不会添加锁。
+ * 如果这个数据没有被更新，当前线程将自己修改的数据成功写入。
+ * 如果数据已经被更新，则执行其他操作,报错或者自动重试。
+ * 通过使用无锁编程来实现CAS：Compare And Swap（比较与交换）算法，Java原子类中的递增操作就是通过CAS自旋实现的。
+ * 乐观锁适合读操作多的场景，不加锁的特点能够使其读操作的性能大幅提升。
+ * @author jy
+ */
 public class OptiLock {
+    public static void main(String[] args){
+        AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.incrementAndGet();
+    }
+
 }
---
+++ src/main/java/cn/com/concept/PessimisticLock.java
@@ -0,0 +1,28 @@
+package main.java.cn.com.concept;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 悲观锁：线程会锁住同步资源。
+ * 悲观锁认为自己在使用数据时一定会有别的线程来修改数据，所以在获取数据时会添加锁，确保数据不会被别的线程修改。
+ * Java中，synchronized关键字和Lock的实现类都是悲观锁。
+ * 悲观锁适合写操作多的场景，先加锁可以保证写操作时数据正确。
+ * @author jy
+ */
+public class PessimisticLock {
+    public synchronized void testMethod(){
+        //同步资源的一系列操作
+    }
+
+    //可重入锁，保证多个线程用的是一个锁
+    private ReentrantLock reentrantLock = new ReentrantLock();
+
+    /**
+     * 模拟修改公共资源
+     */
+    public void modifyPublicResource(){
+        reentrantLock.lock();
+        //操作公共资源
+        reentrantLock.unlock();
+    }
+}
